39eae998c8b27b75f68dc74b1abe3b995d609425
youngji <youngji.jang804@gmail.com>
2023-05-12 23:56:54 +0900

    init data 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into default_file_extension(name, checked)
+values ("bat",false),
+       ("cmd",false),
+       ("com",false),
+       ("cpl",false),
+       ("exe",false),
+       ("scr",false),
+       ("js",false);
